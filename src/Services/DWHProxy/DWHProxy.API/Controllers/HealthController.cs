b62c5f933257142d248d385b85fca8bb50a25739
aleksandar.djenic@devart.rs <aleksandar.djenic@devart.rs>
2022-05-16 11:16:28 +0200

    Health status in DWHProxy

---
+++ src/Services/DWHProxy/DWHProxy.API/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DWHProxy.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("health")]
+        [Produces("text/plain")]
+        public async Task<object> GetHealthStatusInfo()
+        {
+            return Ok("UP");
+        }
+    }
+}
